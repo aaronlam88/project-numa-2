8e0ad4e18d5c22ce2733d122262f31c3ddd5b6eb
Kavish Jain <kavishme@gmail.com>
2017-04-12 19:45:47 -0700

    git update

---
+++ src/gash/router/server/RequestHandler.java
@@ -0,0 +1,81 @@
+package gash.router.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gash.router.container.RoutingConf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import pipe.common.Common.Failure;
+import routing.Pipe.CommandMessage;
+
+public class RequestHandler extends SimpleChannelInboundHandler<CommandMessage> {
+	protected static Logger logger = LoggerFactory.getLogger("cmd");
+	protected RoutingConf conf;
+
+	public RequestHandler(RoutingConf conf) {
+		if (conf != null) {
+			this.conf = conf;
+		}
+	}
+
+	/**
+	 * 
+	 * @param msg
+	 */
+	public void handleMessage(CommandMessage msg, Channel channel) {
+		if (msg == null) {
+			// TODO add logging
+			System.out.println("ERROR: Unexpected content - " + msg);
+			return;
+		}
+
+		PrintUtil.printCommand(msg);
+
+		try {
+			// TODO How can you implement this without if-else statements?
+			if (msg.hasPing()) {
+				logger.info("ping from " + msg.getHeader().getNodeId());
+				//logger.info("ping from " + msg.getMessage());
+			} else if (msg.hasMessage()) {
+				logger.info(msg.getMessage());
+			} else {
+			}
+
+		} catch (Exception e) {
+			// TODO add logging
+			Failure.Builder eb = Failure.newBuilder();
+			eb.setId(conf.getNodeId());
+			eb.setRefId(msg.getHeader().getNodeId());
+			eb.setMessage(e.getMessage());
+			CommandMessage.Builder rb = CommandMessage.newBuilder(msg);
+			rb.setErr(eb);
+			channel.write(rb.build());
+		}
+
+		System.out.flush();
+	}
+
+	/**
+	 * a message was received from the server. Here we dispatch the message to
+	 * the client's thread pool to minimize the time it takes to process other
+	 * messages.
+	 * 
+	 * @param ctx
+	 *            The channel the message was received from
+	 * @param msg
+	 *            The message
+	 */
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, CommandMessage msg) throws Exception {
+		handleMessage(msg, ctx.channel());
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		logger.error("Unexpected exception from downstream.", cause);
+		ctx.close();
+	}
+
+}

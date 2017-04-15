8a9522720cb6a91598e9cf2466380e55e186cf12
Aaron Lam <aaron.y.lam@sjsu.edu>
2017-04-15 11:59:59 -0700

    ServerState.java add Hashtable

--- src/gash/router/server/ServerState.java
+++ src/gash/router/server/ServerState.java
@@ -1,13 +1,19 @@
 package gash.router.server;
 
+import java.util.Hashtable;
+
 import gash.router.container.RoutingConf;
 import gash.router.server.edges.EdgeMonitor;
 import gash.router.server.tasks.TaskList;
+import pipe.common.Common.Chunk;
+import pipe.common.Common.ChunkLocation;
 
 public class ServerState {
 	private RoutingConf conf;
 	private EdgeMonitor emon;
 	private TaskList tasks;
+	
+	private Hashtable<Chunk, ChunkLocation> hashTable;
 
 	public RoutingConf getConf() {
 		return conf;
@@ -31,6 +37,14 @@ public class ServerState {
 
 	public void setTasks(TaskList tasks) {
 		this.tasks = tasks;
+	}
+
+	public Hashtable<Chunk, ChunkLocation> getHashTable() {
+		return hashTable;
+	}
+
+	public void setHashTable(Hashtable<Chunk, ChunkLocation> hashTable) {
+		this.hashTable = hashTable;
 	}
 
 }

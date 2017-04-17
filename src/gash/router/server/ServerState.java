3961193c8ebdfb45524d178994630305edf41f65
Aaron Lam <aaron.y.lam@sjsu.edu>
2017-04-16 18:08:40 -0700

    update

--- src/gash/router/server/ServerState.java
+++ src/gash/router/server/ServerState.java
@@ -13,13 +13,18 @@ import routing.Pipe.CommandMessage;
 import java.util.concurrent.LinkedBlockingDeque;
 
 public class ServerState {
+	// The Log file for the system. Record filename and it location.
+	public static Hashtable<String, LocationList> hashTable = new Hashtable<>();
+	
 	private RoutingConf conf;
 	private EdgeMonitor emon;
 	private TaskList tasks;
+	private String dataPath;
+	
 	private int currentLeader; //current leader node id
 	private boolean isLeader;
-	public static Hashtable<String, LocationList> hashTable = new Hashtable<>();
-	private String dataPath;
+	private int currentTerm;
+	private boolean voted; 
 	
 	// This queue has info about incoming chunks that has been saved to file system
 	// Worker should report this to leader and get logs updated
@@ -37,13 +42,15 @@ public class ServerState {
 		}else{
 			dataPath = Paths.get(".", "data").toAbsolutePath().normalize().toString();
 		}
-		wmforward = new LinkedBlockingDeque<WorkMessage>();
+		
 		incoming = new LinkedList<FileChunkObject>();
-
+		
+		wmforward = new LinkedBlockingDeque<WorkMessage>();
+		cmforward = new LinkedBlockingDeque<CommandMessage>();
+		
 		currentLeader = -1; // unknown
 		isLeader = false; // doesn't assume itself as leader
-		//if both currentLeader is unknown call for election
-		cmforward = new LinkedBlockingDeque<CommandMessage>();
+		//if both currentLeader is unknown and !isLeader call for election
 	}
 	
 	public String getDbPath(){
@@ -88,5 +95,21 @@ public class ServerState {
 
 	public void setLeader(boolean isLeader) {
 		this.isLeader = isLeader;
+	}
+
+	public int getCurrentTerm() {
+		return currentTerm;
+	}
+
+	public void setCurrentTerm(int currentTerm) {
+		this.currentTerm = currentTerm;
+	}
+
+	public boolean isVoted() {
+		return voted;
+	}
+
+	public void setVoted(boolean voted) {
+		this.voted = voted;
 	}
 }
